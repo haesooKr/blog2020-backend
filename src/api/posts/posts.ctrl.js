666bf381c9c5f91e053e378c2d9fb3f50efc1c54
haesooKr <gotn676@gmail.com>
2020-07-02 16:50:33 -0400

    Add pagination

--- src/api/posts/posts.ctrl.js
+++ src/api/posts/posts.ctrl.js
@@ -6,22 +6,22 @@ const { ObjectId } = mongoose.Types;
 
 export const checkObjectId = (ctx, next) => {
   const { id } = ctx.params;
-  if(!ObjectId.isValid(id)){
+  if (!ObjectId.isValid(id)) {
     ctx.status = 400;
     return;
   }
   return next();
-}
+};
 
 export const write = async (ctx) => {
   const schema = Joi.object().keys({
     title: Joi.string().required(),
     body: Joi.string().required(),
     tags: Joi.array().items(Joi.string()).required(),
-  })
+  });
 
   const result = schema.validate(ctx.request.body);
-  if(result.error){
+  if (result.error) {
     ctx.status = 400;
     ctx.body = result.error;
     return;
@@ -42,10 +42,28 @@ export const write = async (ctx) => {
 };
 
 export const list = async (ctx) => {
+  const page = parseInt(ctx.query.page || '1', 10);
+
+  if (page < 1) {
+    ctx.status = 400;
+    return;
+  }
+
   try {
-    const posts = await Post.find().exec();
-    ctx.body = posts;
-  } catch (e){
+    const posts = await Post.find()
+      .sort({ _id: -1 })
+      .limit(10)
+      .skip((page - 1) * 10)
+      .lean()
+      .exec();
+    const postCount = await Post.countDocuments().exec();
+    ctx.set('Last-Page', Math.ceil(postCount / 10));
+    ctx.body = posts.map((post) => ({
+      ...post,
+      body:
+        post.body.length < 200 ? post.body : `${post.body.slice(0, 200)}...`,
+    }));
+  } catch (e) {
     ctx.throw(500, e);
   }
 };
@@ -54,7 +72,7 @@ export const read = async (ctx) => {
   const { id } = ctx.params;
   try {
     const post = await Post.findById(id).exec();
-    if(!post){
+    if (!post) {
       ctx.status = 404;
       return;
     }
@@ -70,7 +88,7 @@ export const remove = async (ctx) => {
     const post = await Post.findByIdAndRemove(id).exec();
     ctx.status = 204;
     ctx.body = post;
-  } catch (e){
+  } catch (e) {
     ctx.throw(500, e);
   }
 };
@@ -84,17 +102,17 @@ export const update = async (ctx) => {
   });
 
   const result = schema.validate(ctx.request.body);
-  if(result.error){
+  if (result.error) {
     ctx.status = 400;
     ctx.body = result.error;
     return;
   }
-  
+
   try {
     const post = await Post.findByIdAndUpdate(id, ctx.request.body, {
       new: true,
     }).exec();
-    if(!post){
+    if (!post) {
       ctx.status = 404;
       return;
     }
