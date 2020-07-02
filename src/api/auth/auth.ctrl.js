a3d7a41f8d7e87a89305c945592dc10201db342a
haesooKr <gotn676@gmail.com>
2020-07-02 17:26:57 -0400

    Add generateToken method

--- src/api/auth/auth.ctrl.js
+++ src/api/auth/auth.ctrl.js
@@ -26,11 +26,45 @@ export const register = async (ctx) => {
     await user.setPassword(password);
     await user.save();
     ctx.body = user.serialize();
+
+    const token = user.generateToken();
+    ctx.cookies.set('access_token', token, {
+      maxAge: 1000 * 60 * 60 * 24 * 7,
+      httpOnly: true,
+    });
   } catch (e) {
     ctx.throw(500, e);
   }
 };
 
-export const login = async (ctx) => {};
+export const login = async (ctx) => {
+  const { username, password } = ctx.request.body;
+
+  if (!username || !password) {
+    ctx.status = 401;
+    return;
+  }
+
+  try {
+    const user = await User.findByUsername(username);
+    if (!user) {
+      ctx.status = 401;
+      return;
+    }
+    const valid = await user.checkPassword(password);
+    if (!valid) {
+      ctx.status = 401;
+      return;
+    }
+    ctx.body = user.serialize();
+    const token = user.generateToken();
+    ctx.cookies.set('access_toekn', token, {
+      maxAge: 1000 * 60 * 60 * 24 * 7,
+      httpOnly: true,
+    });
+  } catch (e) {
+    ctx.throw(500, e);
+  }
+};
 export const check = async (ctx) => {};
 export const logout = async (ctx) => {};
