a07e02aeac3812d4fa619b6bad4cb49b11bdb6ce
haesooKr <gotn676@gmail.com>
2020-07-02 17:46:04 -0400

    Autoupdate token for users

--- src/lib/jwtMiddleware.js
+++ src/lib/jwtMiddleware.js
@@ -1,7 +1,7 @@
 import jwt from 'jsonwebtoken';
+import User from '../models/user';
 
-const jwtMiddleware = (ctx, next) => {
-  console.log('jwt 미들웨어 실행');
+const jwtMiddleware = async (ctx, next) => {
   const token = ctx.cookies.get('access_token');
   if (!token) return next();
   try {
@@ -10,6 +10,15 @@ const jwtMiddleware = (ctx, next) => {
       _id: decoded._id,
       username: decoded.username,
     };
+    const now = Math.floor(Date.now() / 1000);
+    if (decoded.exp - now < 60 * 60 * 24 * 3.5) {
+      const user = await User.findById(decoded._id);
+      const token = user.generateToken();
+      ctx.cookies.set('access_token', token, {
+        maxAge: 1000 * 60 * 60 * 24 * 7,
+        httpOnly: true,
+      });
+    }
     return next();
   } catch (e) {
     return next();
